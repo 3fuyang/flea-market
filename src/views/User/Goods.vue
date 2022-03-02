0287472079ed86015b3a8aa3fe33a720bd9bffe2
fwio <624835339@qq.com>
2022-03-02 20:22:18 +0800

    适配商品列表组件。

--- src/views/User/Goods.vue
+++ src/views/User/Goods.vue
@@ -1,41 +1,49 @@
 <template>
-<div class="goods">
-	<el-tabs type="border-card" stretch>
-        <el-tab-pane label="销售中">
-            <GoodCardList :hasSaled="false"/>
-        </el-tab-pane>
-        <el-tab-pane label="卖掉的">
-            <GoodCardList :hasSaled="true"/>
-        </el-tab-pane>
-        <el-tab-pane label="上架商品">
-            <GoodInfo/>
-        </el-tab-pane>
-    </el-tabs>
+<div class="root-wrapper">
+<span class="title">商品管理</span><br/>  
+  <el-row>
+    <el-col :span="6"></el-col>
+    <el-col :span="12">
+      <div class="tab-wrapper">
+        <el-tabs v-model="currentTab" class="tabs" type="border-card" stretch>
+          <el-tab-pane label="上架中" name="onShelf" lazy>
+            <GoodsListTable v-if="currentTab === 'onShelf'" :userId="uid" :goodsStatus="currentTab"/>
+          </el-tab-pane>
+          <el-tab-pane label="已售出" name="soldOut">
+            <GoodsListTable v-if="currentTab === 'soldOut'" :userId="uid" :goodsStatus="currentTab"/>
+          </el-tab-pane>
+          <el-tab-pane label="新添闲置" name="uploadGood">
+
+          </el-tab-pane>
+        </el-tabs>
+      </div>
+    </el-col>
+    <el-col :span="6"></el-col>
+  </el-row>
 </div>
 </template>
 
+<script setup>
+import { onMounted, ref } from 'vue';
+import GoodsListTable from '../../components/Goods/GoodsListTable.vue';
+const currentTab = ref('onShelf');
+const uid = ref(null);
+
+onMounted(()=>{
+  // 获取用户ID
+  uid.value = window.sessionStorage.getItem('uid');
+})
+</script>
+
 <style scoped>
-::v-deep .el-tabs__nav-scroll{
-	width:20%;
-	margin:0 auto
+.title{
+  display: inline-block;
+  color: #000000;
+  margin: 20px 0px 10px 20px;
+  font-size: 24px;
+  font-weight: bold;
+}  
+.tab-wrapper{
+  margin-bottom: 20px;
 }
 </style>
-
-<script>
-import GoodCardList from '@/components/GoodCardList.vue'
-import GoodInfo from '@/components/GoodInfo.vue'
-export default {
-    components:{
-        GoodCardList,
-        GoodInfo
-    },
-    data(){
-        return{
-
-        }
-    },
-    methods:{
-
-    },
-}
-</script>

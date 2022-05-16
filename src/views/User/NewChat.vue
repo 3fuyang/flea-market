091f0b9ca5233ab04fdf6b3f4cddd97476c9d25b
fwio <624835339@qq.com>
2022-05-16 13:15:34 +0800

    完成新聊天窗口样式

--- src/views/User/NewChat.vue
+++ src/views/User/NewChat.vue
@@ -1,12 +1,19 @@
 <script lang="ts" setup>
 import SunsetCity from '@/components/CSSDoodle/SunsetCity.vue'
-import { NDivider, NText, NAvatar, NCard, NIcon, useMessage, NInput, NScrollbar, NEllipsis } from 'naive-ui'
-import { ChatboxEllipsesOutline, Home, LogoGithub, SearchOutline } from '@vicons/ionicons5'
+import { NButton, NInput, NTag, NDivider, NText, NAvatar, NCard, NIcon, useMessage, NScrollbar, NEllipsis } from 'naive-ui'
+import { Image, ChatboxEllipsesOutline, Home, LogoGithub, SearchOutline } from '@vicons/ionicons5'
+import { Emoji16Filled } from '@vicons/fluent'
 import { useRouter } from 'vue-router'
+import { ref } from 'vue'
+import axios from 'axios'
+import { useUserStore } from '@/stores/user'
+import { storeToRefs } from 'pinia'
 
 const router = useRouter()
 const message = useMessage()
-
+const userStore = useUserStore()
+const { userID } = storeToRefs(userStore)
+ 
 // NavBar 中按钮
 const menuItems = [
   {
@@ -43,6 +50,31 @@ const oponentCardStyle = {
   alignItems: 'center',
   gap: '.5em'
 }
+
+// 消息类型
+interface Message {
+  day_time: string
+  speaker: number
+  details: string
+}
+
+// 消息数据
+const messageData = ref<Message[]>([])
+axios.get(`/api/getMessage/${userID.value}/1951002`)
+  .then((res) => {
+    let isSelfA = true
+    res.data.forEach((item: any) => {
+      messageData.value.push({
+        day_time: item.date_time.substr(0, 19).replace('T', ' '),
+        // 0 表示对方，1 表示该用户
+        speaker: isSelfA ? (item.speaker === 0 ? 1 : 0) : (item.speaker === 1 ? 1: 0),
+        details: item.details
+      })
+    })
+  })
+
+// 消息输入框model对象
+const textarea = ref('')
 </script>
 
 <template>
@@ -123,10 +155,75 @@ const oponentCardStyle = {
       </n-text>
 
       <n-divider class="divider"/>
-      <!--消息窗口-->
-      <n-scrollbar style="max-height: 48em;">
-
+      <!--消息列表-->
+      <n-scrollbar
+        style="max-height: 29.2em;"
+        class="message-list">
+        <div
+          v-for="item in messageData"
+          class="message-item">
+          <n-tag
+            class="time-tag"
+            round
+            size="small"
+            :color="{ color: '#E39BB6', textColor: '#FFF'}">
+            {{item.day_time}}
+          </n-tag>          
+          <div class="message-row">
+            <div
+              v-if="item.speaker !== 0"
+              class="polyfill"/>
+            <div
+              :class="item.speaker === 0 ? 'oponent-bubble' : 'self-bubble'">
+              {{item.details}}
+            </div>
+            <div
+              v-if="item.speaker === 0"
+              class="polyfill"/>
+          </div>
+        </div>
       </n-scrollbar>
+
+      <n-divider class="divider"/>
+      <!--输入框-->
+      <n-input
+        class="message-input"
+        type="textarea"
+        placeholder="Type your message."
+        :autosize="{ minRows: 4, maxRows: 4 }"
+        clearable
+        maxlength="200" 
+        show-count
+        v-model:value="textarea"/>
+      <!--聊天工具栏-->
+      <div class="tool-bar">
+        <n-icon
+          class="emoji-icon"
+          depth="4"
+          size="24">
+          <emoji16-filled/>
+        </n-icon>
+        <n-icon
+          class="emoji-icon"
+          depth="4"
+          size="24">
+          <Image/>
+        </n-icon>        
+        <div class="polyfill"/>
+        <n-button
+          size="small"
+          round
+          class="btn">
+          关闭
+        </n-button>        
+        <n-button
+          size="small"
+          round
+          color="#7BC4EF"
+          class="btn">
+          发送
+        </n-button>
+      </div> 
     </div>
   </n-card>
 </template>
@@ -137,6 +234,9 @@ $shallow: #F7E3EA;
 $smoke: #F3F3F3;
 $grey: #aaa;
 $purple: #B062A9;
+$green: #55D2CA;
+$deep-green: hsl(176,50%,50%);
+$blue: #7BC4EF;
 
 .chat-frame {
   box-sizing: border-box;
@@ -236,6 +336,120 @@ $purple: #B062A9;
 .divider {
   margin: .15em 0;
   margin-bottom: .5em;
+}
+
+.message-list {
+  display: flex;
+  flex-direction: column;
+}
+
+.message-item {
+  display: flex;
+  flex-direction: column;
+}
+
+.time-tag {
+  align-self: center;
+  margin-bottom: .8em;
+}
+
+
+.message-row {
+  display: flex;
+}
+
+.oponent-bubble {
+  max-width: 16em;
+  align-self: flex-start;
+  border-radius: .6em;  
+  margin-left: 1em;
+  font-size: 1em;
+  background-color: $green;
+  text-align: left;
+  word-wrap: break-word;
+  padding: .5em;
+  color: white;
+  transition: box-shadow .4s ease-in-out;
+  position: relative;
+  &:not(:last-child) {
+    margin-bottom: .4em;
+  }
+  &:hover {
+    box-shadow: 0 7px 5px -5px $grey;
+  }
+  &::before {
+    content: "";
+    position: absolute;
+    left: -.6em;
+    bottom: 0;
+    border: .8em solid $green;
+    border-top-color: transparent;
+    border-left-color: transparent;
+    border-right-color: transparent;
+  }
+}
+
+.self-bubble {
+  max-width: 16em;
+  align-self: flex-end;
+  border-radius: .6em;  
+  margin-right: 1em;
+  font-size: 1em;
+  background-color: $blue;
+  text-align: left;
+  word-wrap: break-word;
+  padding: .5em;
+  color: white;
+  transition: box-shadow .4s ease-in-out;
+  position: relative;
+  &:not(:last-child) {
+    margin-bottom: .4em;
+  }
+  &:hover {
+    box-shadow: 0 7px 5px -5px $grey;
+  }
+  &::before {
+    content: "";
+    position: absolute;
+    right: -.6em;
+    bottom: 0;
+    border: .8em solid $blue;
+    border-top-color: transparent;
+    border-left-color: transparent;
+    border-right-color: transparent;
+  }
+}
+
+.polyfill {
+  flex-grow: 1;
+}
+
+.message-input {
+  text-align: left;
+  margin-top: .3em;
+}
+
+.tool-bar {
+  display: flex;
+  gap: 1em;
+  height: 2em;
+  margin-top: .6em;
+  align-items: center;
+  width: 100%;
+  justify-content: flex-start;
+}
+
+.btn {
+  align-self: flex-end;
+}
+
+.emoji-icon {
+  align-self: flex-start;
+  cursor: pointer;
+  transition: box-shadow .2s ease-out;
+  &:hover, &:active {
+    box-shadow: 0 0 2px $grey;
+  }
 }
 
 @keyframes blink {

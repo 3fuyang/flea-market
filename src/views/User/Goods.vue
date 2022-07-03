40c99690c3eda61aca45f88e287c950184a7348f
fwio <624835339@qq.com>
2022-07-03 22:57:07 +0800

    商品管理页移除element plus

--- src/views/User/Goods.vue
+++ src/views/User/Goods.vue
@@ -1,47 +1,40 @@
 <template>
-  <div class="root-wrapper">
-    <page-title>
-      <template #icon>
-        <GiftCard20Regular />
-      </template>
-      <template #main-title>
-        商品管理
-      </template>
-      <template #sub-title>
-        My Unused
-      </template>
-    </page-title>
-    <el-row>
-      <el-col :span="6" />
-      <el-col :span="12">
-        <div class="tab-wrapper">
-          <el-tabs v-model="currentTab" class="tabs" type="border-card" stretch>
-            <el-tab-pane label="上架中" name="onShelf" lazy>
-              <NewGoodListTable v-if="currentTab === 'onShelf'" :userId="userID" :goodsStatus="currentTab"
-                @check-info="checkGoodInfo" />
-            </el-tab-pane>
-            <el-tab-pane label="已售出" name="soldOut" lazy>
-              <NewGoodListTable v-if="currentTab === 'soldOut'" :userId="userID" :goodsStatus="currentTab"
-                @check-info="checkGoodInfo" />
-            </el-tab-pane>
-            <el-tab-pane label="新添闲置" name="uploadGood" lazy>
-              <UploadGoodModal v-if="currentTab === 'uploadGood'" :userId="userID" />
-            </el-tab-pane>
-          </el-tabs>
-        </div>
-      </el-col>
-      <el-col :span="6" />
-    </el-row>
-    <Teleport to="main">
-      <GoodsInfoModal v-if="showInfoModal" :goodId="currentGoodId.toString()" :status="currentTab" :show="showInfoModal"
-        @close="closeGoodInfo" />
-    </Teleport>
+  <page-title>
+    <template #icon>
+      <GiftCard20Regular />
+    </template>
+
+    <template #main-title>
+      商品管理
+    </template>
+
+    <template #sub-title>
+      My Unused
+    </template>
+  </page-title>
+  <div class="tab-wrapper">
+    <el-tabs v-model="currentTab" class="tabs" type="border-card" stretch>
+      <el-tab-pane label="上架中" name="onShelf" lazy>
+        <NewGoodListTable v-if="currentTab === 'onShelf'" :userId="userID" :goodsStatus="currentTab"
+          @check-info="checkGoodInfo" />
+      </el-tab-pane>
+      <el-tab-pane label="已售出" name="soldOut" lazy>
+        <NewGoodListTable v-if="currentTab === 'soldOut'" :userId="userID" :goodsStatus="currentTab"
+          @check-info="checkGoodInfo" />
+      </el-tab-pane>
+      <el-tab-pane label="新添闲置" name="uploadGood" lazy>
+        <UploadGoodModal v-if="currentTab === 'uploadGood'" :userId="userID" />
+      </el-tab-pane>
+    </el-tabs>
   </div>
+  <Teleport to="main">
+    <GoodsInfoModal v-if="showInfoModal" :goodId="currentGoodId.toString()" :status="currentTab" :show="showInfoModal"
+      @close="closeGoodInfo" />
+  </Teleport>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
-import GoodsListTable from '../../components/Goods/GoodsListTable.vue'
 import UploadGoodModal from '../../components/Goods/UploadGoodModal.vue'
 import GoodsInfoModal from '../../components/Goods/GoodsInfoModal.vue'
 import { useUserStore } from '@/stores/user'
@@ -74,10 +67,6 @@ function closeGoodInfo() {
 </script>
 
 <style scoped>
-.root-wrapper {
-  width: 100%;
-}
-
 .title {
   display: inline-block;
   color: #000000;
@@ -88,5 +77,8 @@ function closeGoodInfo() {
 
 .tab-wrapper {
   margin-bottom: 20px;
+  justify-self: center;
+  width: 50%;
+  z-index: 1;
 }
 </style>
